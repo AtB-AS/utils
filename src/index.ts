a19cecf233d6464ffdaf745785ab7f29ddb3f241
Håkon Strandlie <hakon.strandlie@atb.no>
2025-05-28 14:57:28 +0200

    chore: Fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from './fare-contract';
 export {type ErrorResponse, type HttpError} from './error-response';
-export {type BookingAvailabilityType} from './offers/booking'
+export {type BookingAvailabilityType} from './offers/booking';
