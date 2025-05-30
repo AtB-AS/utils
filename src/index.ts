58db3cadd793c83c7dbeb368e2b8645f047a8b73
Håkon Strandlie <hakon@strandlie.co>
2025-05-30 15:43:41 +0200

    feat: Added BookingAvailabilityType (#19)
    * feat: Added BookingAvailabilityType
    
    * chore: Fixed formatting
    
    * refactor: Removed type export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './fare-contract';
 export * from './ticket-offer';
 export {ErrorResponse, HttpError} from './error-response';
+export {BookingAvailabilityType} from './offers/booking';
---
+++ src/offers/booking.ts
@@ -0,0 +1,12 @@
+export enum BookingAvailabilityType {
+  /** The product supports booking and has available seats */
+  Available = 'available',
+  /** The product does not support booking */
+  BookingNotSupported = 'booking_not_supported',
+  /** No more seats are available for booking */
+  SoldOut = 'sold_out',
+  /** The product supports booking, but ticket sale isn't currently open */
+  Closed = 'closed',
+  /** Fallback state for unhandled errors */
+  Unknown = 'unknown',
+}
