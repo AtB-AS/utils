e320ccd95e6948d24c052a559f85871ac31812a8
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-09-25 11:04:03 +0200

    fix: export through index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './fare-contract';
 export * from './offers/ticket-offer';
 export {ErrorResponse, HttpError} from './error-response';
 export {BookingAvailabilityType} from './offers/booking';
+export {formatNumberToString} from './utils';
